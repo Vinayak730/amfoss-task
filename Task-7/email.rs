53292f99a66fb53f24ac5639a66ce1c70a0519ae
Vinayak730 <52862736+Vinayak730@users.noreply.github.com>
2019-08-19 19:55:01 +0530

    Add files via upload

---
+++ Task-7/email.rs
@@ -0,0 +1,16 @@
+extern crate regex;
+use std::io;
+use regex::Regex;
+
+
+fn main() {
+	let mut input = string::new();
+	io::stdin().read_line(&mut input)?;
+	        ok()
+	let re = Regex::new(r"^\w+@[[:word:]]+\.[[:word:]]$").unwrap();
+	
+	  match re.capture(input){
+	      some(caps) => println!("valid mail"),
+	      None => println!("Invalid mail")
+	  }        
+}
