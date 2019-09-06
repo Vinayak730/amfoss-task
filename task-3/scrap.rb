ba487c9b091aac9968d4c79756a4989cc7f22735
Vinayak730 <52862736+Vinayak730@users.noreply.github.com>
2019-09-06 21:53:23 +0530

    Add files via upload

---
+++ task-3/scrap.rb
@@ -0,0 +1,12 @@
+require "nokogiri"
+require "open-uri"
+
+puts "enter the search content"
+
+title = gets
+
+ doc = Nokogiri::HTML(open('https://www.google.com/search?start=0&end=11&q='+title))
+
+doc.xpath('*/a/div[text()]').each do |linux|
+ puts linux.content
+end
